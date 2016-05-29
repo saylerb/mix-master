d8314c84683ad68692b7c51b8952e93c6207d72b
saylerb <sayler.b@gmail.com>
2016-05-29 11:51:58 -0600

    add to db seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+#
+#
+artist_name = "Bob Marley"
+artist_image_path =  "http://cps-static.rovicorp.com/3/JPG_400/MI0003/146/MI0003146038.jpg"
+Artist.create(name: artist_name, image_path: artist_image_path)
+
+
