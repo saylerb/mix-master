42fb3f1798f0afd46a25026ee491e28bd276089e
saylerb <sayler.b@gmail.com>
2016-05-30 15:45:22 -0600

    generate playlist model

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   end
 
   resources :songs, only: [:show]
-  resources :playlists, only: [:index]
+  resources :playlists, only: [:index, :new, :create]
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
