03fef16878f72076bd5b91efc01c481d9d1d1365
saylerb <sayler.b@gmail.com>
2016-05-30 16:02:31 -0600

    generate PlaylistSong model for join table

--- spec/models/playlist_spec.rb
+++ spec/models/playlist_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Playlist, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  # shoulda-matchers
+
+  it { should have_many(:playlist_songs) }
+  it { should have_many(:songs).through(:playlist_songs) }
+
 end
