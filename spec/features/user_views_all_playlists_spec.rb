b1745dd676d34e312b82688245791de53f6ec8b7
saylerb <sayler.b@gmail.com>
2016-05-30 17:36:58 -0600

    add playlist factory and playlist index feature test

--- spec/features/user_views_all_playlists_spec.rb
+++ spec/features/user_views_all_playlists_spec.rb
@@ -5,10 +5,16 @@ RSpec.feature "User views all playlists" do
   scenario "User sees a list of all playlists when visiting index page" do 
   # As a user
   # Given that playlists exist in the database
-
+  playlist = create(:playlist)
+  
   # When I visit the playlists index
+  visit playlists_path
+  
   # Then I should see each playlist's name
+  expect(page).to have_content(playlist.name)
+  
   # And each name should link to that playlist's individual page
+  expect(page).to have_link(playlist.name), href: playlist_path(playlist)
 
   end
 end
