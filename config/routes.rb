a4b740b5dec1988127adbf7b19aaa7d8ad170fe1
saylerb <sayler.b@gmail.com>
2016-05-30 15:18:11 -0600

    add factory for playlist

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   end
 
   resources :songs, only: [:show]
+  resources :playlists, only: [:index]
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
--- spec/support/factories.rb
+++ spec/support/factories.rb
@@ -1,6 +1,24 @@
 FactoryGirl.define do
   factory :artist do
-    name "Justin Beiber"
+    name
     image_path "http://cdn.baeblemusic.com/images/bblog/11-10-2015/justinbieberblogpost3-580.jpg"
   end
+
+  sequence :name do |n|
+    "#{n} Artist"
+  end
+
+  sequence :title, ["A", "C", "B"].cycle do |n|
+    "#{n} Title"
+  end
+
+  factory :song do
+    title
+    artist
+  end
+
 end
+
+
+
+
