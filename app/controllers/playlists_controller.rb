7853804405f98ecf4b90ea7bbfd10f4ba4b8a353
saylerb <sayler.b@gmail.com>
2016-05-30 15:54:06 -0600

    correct checkboxes in view

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -5,4 +5,10 @@ class PlaylistsController < ApplicationController
   def new
     @playlist = Playlist.new
   end
+
+  def create
+    @playlist = Playlist.create(name: params[:playlist][:name],
+                             songs_ids: params[:playlist][:song_ids])
+    redirect_to playlist_path(@playlist)
+  end
 end
